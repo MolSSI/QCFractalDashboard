d2f0a332970db4da3e970f5e81e6b0711e1644c4
John Gentle <taoteg@gmail.com>
2020-06-08 13:54:06 -0500

    Added the python-shell npm package. Updated renderer.js to use the shell package afor IPC Test B.

--- src/client/renderer.js
+++ src/client/renderer.js
@@ -1,23 +1,41 @@
 function sendToPython() {
 
-  var python = require('child_process').spawn('python', ['../server/calc.py', input.value]);
+    // IPC Test A..
+    var python = require('child_process').spawn('python', ['../server/calc.py', input.value]);
 
-  python.stdout.on('data', function (data) {
-    console.log("Python response: ", data.toString('utf8'));
-    result.textContent = data.toString('utf8');
-  });
+    // IPC Test B.
+    var { PythonShell } = require('python-shell');
 
-  python.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-  });
+    let options = {
+      mode: 'text',
+      args: [input.value]
+    };
 
-  python.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
-  });
+    // IPC Test A.
+    python.stdout.on('data', function(data) {
+        console.log("Python response: ", data.toString('utf8'));
+        result.textContent = data.toString('utf8');
+    });
+
+    python.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
+    python.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
+
+    // IPC Test B.
+    PythonShell.run('../server/calc.py', options, function (err, results) {
+      if (err) throw err;
+      // results is an array consisting of messages colected during execution.
+      console.log('results: ', results);
+      result.textCOntent =results[0];
+    });
 }
 
 btn.addEventListener('click', () => {
-  sendToPython();
+    sendToPython();
 });
 
 btn.dispatchEvent(new Event('click'));
