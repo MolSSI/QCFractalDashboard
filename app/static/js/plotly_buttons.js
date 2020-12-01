e01ae72fc5f67f2fb36ed21216c35fcb561feb25
Eman Hussein <emanhussein@Emans-MacBook-Air.local>
2020-12-01 15:33:17 -0500

    Multiselect Dropdown added using selectize

--- app/static/js/plotly_buttons.js
+++ app/static/js/plotly_buttons.js
@@ -1,6 +1,5 @@
 
 $(document).ready(function () {
-  // $('#hostnamesdata').multiselect();
 
   var lineDiv = document.getElementById('myDiv2');
 
@@ -13,6 +12,7 @@ $(document).ready(function () {
   var clusters_names = [];
   var da;
   var layout2;
+  var clusters_active_tasks = [];
   $.ajax
     (
       {
@@ -38,7 +38,11 @@ $(document).ready(function () {
             clusters_names.push(data_ret[i].cluster)
             completed.push(data_ret[i].completed)
             failures.push(data_ret[i].failures)
-            // submitted.push("Submitted"
+
+            if (data_ret[i].active_tasks != null) {
+              clusters_active_tasks.push(data_ret[i].active_tasks)
+            }
+
           }
           ////////////////////////////////////// hostnames dropdown, the way countries example was made////////////////////////////////////////////////////////////////////////
           var select = document.getElementById("hostnamesdata");
@@ -113,8 +117,13 @@ $(document).ready(function () {
             var layout = {
               title: 'Completed & Failed Tasks Plot of Selected Hostname',
 
-              height: 400,
-              width: 480
+              height: 400,//in pixels
+              width: 480, //in pixels
+              yaxis:
+              {
+                rangemode: 'tozero'
+              }
+
             };
 
             Plotly.newPlot('hostnamesDiv', d_completed_failures, layout);
@@ -154,7 +163,7 @@ $(document).ready(function () {
 
             clustername_completed = {};
             clustername_failed = {};
-            clustername_active_tasks={};
+            clustername_active_tasks = {};
 
             for (var i = 0; i < data_ret.length; i++) {
               if (data_ret[i].cluster === chosenClustername) {
@@ -163,12 +172,12 @@ $(document).ready(function () {
                 if (clustername_completed[chosenClustername] != null) {
                   clustername_completed[chosenClustername] = clustername_completed[chosenClustername] + data_ret[i].completed;
                   clustername_failed[chosenClustername] = clustername_failed[chosenClustername] + data_ret[i].failures;
-                  clustername_active_tasks[chosenClustername]=clustername_active_tasks[chosenClustername] +data_ret[i].active_tasks;
+                  clustername_active_tasks[chosenClustername] = clustername_active_tasks[chosenClustername] + data_ret[i].active_tasks;
                 }
                 else {
                   clustername_completed[chosenClustername] = data_ret[i].completed;
                   clustername_failed[chosenClustername] = data_ret[i].failures;
-                  clustername_active_tasks[chosenClustername]= data_ret[i].active_tasks;
+                  clustername_active_tasks[chosenClustername] = data_ret[i].active_tasks;
                 }
 
               }//end of if (data_ret[i].cluster === chosenClustername)
@@ -216,19 +225,23 @@ $(document).ready(function () {
             var layout = {
               title: 'Completed & Failed Tasks Plot of Selected Clusternames',
 
-              height: 400,
-              width: 480
+              height: 400, //in pixels
+              width: 480 //in pixels
             };
             var layout_active_tasks = {
               title: 'Active Tasks Plot of Selected Clusternames',
 
-              height: 300,
-              width: 480
+              height: 300,//in pixels
+              width: 480,//in pixels
+              yaxis:
+              {
+                rangemode: 'tozero'
+              }
             };
 
             Plotly.newPlot('clusternamesDiv', d_completed_failures, layout);
-            Plotly.newPlot('clusternamesDiv_active_tasks', d_active_tasks, layout_active_tasks);
-            
+            // Plotly.newPlot('clusternamesDiv_active_tasks', d_active_tasks, layout_active_tasks);
+
           };
 
           var innerContainer = document.querySelector('[data-num="2"'),
@@ -242,6 +255,134 @@ $(document).ready(function () {
 
 
           ////////////////////////////////////////////////// End of Clusternames dropdown ////////////////////////////////////////////////////////////////
+          ////////////////////////////////////////////////// Dropdown for Cluster with active tasks //////////////////////////////////////////////////
+          var select = document.getElementById("clusternamesdata_active_tasks");
+          var options_clusters_active_tasks = [...new Set(clusters_active_tasks)]; //changed this to cluster names to compare
+          options_clusters = options_clusters.sort(function (a, b) {
+            return a.toLowerCase().localeCompare(b.toLowerCase());
+          });
+          //Populate dropdown of hostnames available 
+          for (var i = 0; i < options_clusters_active_tasks.length; i++) {
+            var opt = options_clusters_active_tasks[i];
+            // console.log(opt);
+            var el = document.createElement("option");
+            el.textContent = opt;
+            el.value = opt;
+            select.appendChild(el);
+          }
+
+          ////////////////////////////////////////////////// End of Dropdown for Cluster with active tasks //////////////////////////////////////////////////
+          // initialize selectize for box plot
+          var $selectize = $('#clusternamesdata').selectize({
+            options_clusters,
+            plugins: ['remove_button'],
+            maxItems: null,
+          });
+
+          var clusternamesDiv = document.getElementById("clusternamesDiv");
+
+          // var plotlyData = [{
+          //   histfunc: "count",
+          //   x: options_clusters,
+          //   type: "histogram",
+          //   name: "Count"
+          // }
+          // ]
+          // console.log("plotlyData");
+          // console.log(plotlyData);
+          // var layout_clusters = {
+          //   title: 'Clusternames',
+
+          //   height: 300,//in pixels
+          //   width: 480,//in pixels
+          //   yaxis:
+          //   {
+          //     rangemode: 'tozero'
+          //   }
+          // };
+          // // plotly initialize
+          // Plotly.newPlot(clusternamesDiv, plotlyData,layout_clusters);
+
+
+          // function updatePlot(chosenClusters) {
+          //   console.log("chosenClusters ya gama3a")
+          //   console.log(chosenClusters)
+          //   var chosenClusters = chosenClusters || []
+          //   // var traceIndex = getActiveTrace()
+
+          //   for (var i = 0; i < chosenClusters.length; i++) {
+          //     plotlyData.push(chosenClusters[i])
+          //   }
+          //   Plotly.update(clusternamesDiv,plotlyData )
+          //   // Plotly.react(clusternamesDiv, plotlyData)
+
+          // };
+
+          // var selectize = $selectize[0].selectize;
+          // show current values in multi input dropdown ////////////////////////
+          // $('select.selectized,input.selectized').each(function () {
+          //   console.log("choosen clustername")
+          //   var $input = $(this);
+
+          //   var update = function (e) {
+          //     var selectedClusters = $input.val();
+          //     console.log("type(selectedClusters)")
+          //     console.log(type(selectedClusters))
+          //     console.log(selectedClusters)
+          //     if (selectedClusters) {
+          //       var chosenClusters = [];
+          //       for (var i = 0; i < selectedClusters.length; i++) {
+          //         chosenClusters.push(selectedClusters[i]);
+          //       }
+          //       updatePlot(chosenClusters)
+
+          //     }
+
+          //     $(this).on('change', update);
+          //   }
+          // });
+
+          // $('#select-to').selectize({
+          //   persist: false,
+          //   maxItems: null, //The max number of items the user can select. 1 makes the control mono-selection, null allows an unlimited number of items.	
+          //   valueField: 'cluster_name', //The name of the property to use as the value when an item is selected.	
+          //   labelField: 'cluster_name', //The name of the property to render as an option / item label (not needed when custom rendering functions are defined).	
+          // searchField: ['cluster_name'],
+          // options: options_clusters,
+          // render: {
+          //   item: function (item, escape) {
+          //     return '<div>' +
+          //       (item.name ? '<span class="name">' + escape(item.name) + '</span>' : '') +
+          //       (item.email ? '<span class="email">' + escape(item.email) + '</span>' : '') +
+          //       '</div>';
+          //   },
+          //   option: function (item, escape) {
+          //     var label = item.name || item.email;
+          //     var caption = item.name ? item.email : null;
+          //     return '<div>' +
+          //       '<span class="label">' + escape(label) + '</span>' +
+          //       (caption ? '<span class="caption">' + escape(caption) + '</span>' : '') +
+          //       '</div>';
+          //   }
+          // },
+          // createFilter: function (input) {
+          //   var match, regex;
+
+          //   // email@address.com
+          //   regex = new RegExp('^' + REGEX_EMAIL + '$', 'i');
+          //   match = input.match(regex);
+          //   if (match) return !this.options.hasOwnProperty(match[0]);
+
+          //   // name <email@address.com>
+          //   regex = new RegExp('^([^<]*)\<' + REGEX_EMAIL + '\>$', 'i');
+          //   match = input.match(regex);
+          //   if (match) return !this.options.hasOwnProperty(match[2]);
+
+          //   return false;
+          // },
+          //   create: false
+          // });
+          ////////////////////////////////////////////////////////////////////////////////////////////////////
           // completed_failures_submitted = Array.prototype.push.apply(completed, failures, submitted)
           // console.log(parsedJson)
           da = [{
@@ -439,26 +580,9 @@ $(document).ready(function () {
           lineDiv.on('plotly_click', function (pointSelect) {
             alert(pointSelect)
 
-            // console.log(eventData.points)
-            // var x = [];
-            // var y = [];
-
-            // var colors = [];
-            // for(var i = 0; i < N; i++) colors.push(color1Light);
-
-            // console.log(eventData.points)
-
-            // eventData.points.forEach(function(pt) {
-            //   x.push(pt.x);
-            //   y.push(pt.y);
-            //   colors[pt.pointNumber] = color1;
-            // });
-
           });
 
           //////////////////////////////////////////////////////////////////////////////
-
-
 
           // plot_data(da, lineDiv, "Hostnames")
           // plot_data(d_active_inactive, lineDiv2, "Status")
@@ -483,318 +607,3 @@ $(document).ready(function () {
   // });
 
 }); //end of: $(document).ready
-//////////////////////////////////////// Countries GDP example /////////////////////////////////////////////////////////////////////////////////////////////////
-// Plotly.d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv', function (err, rows) {
-
-//   function unpack(rows, key) {
-//     return rows.map(function (row) { return row[key]; });
-//   }
-
-//   var allCountryNames = unpack(rows, 'country'),
-//     allYear = unpack(rows, 'year'),
-//     allGdp = unpack(rows, 'gdpPercap'),
-//     listofCountries = [],
-//     currentCountry,
-//     currentGdp = [],
-//     currentYear = [];
-
-//   for (var i = 0; i < allCountryNames.length; i++) {
-//     if (listofCountries.indexOf(allCountryNames[i]) === -1) {
-//       listofCountries.push(allCountryNames[i]);
-//     }
-//   }
-
-//   function getCountryData(chosenCountry) {
-//     currentGdp = [];
-//     currentYear = [];
-//     for (var i = 0; i < allCountryNames.length; i++) {
-//       if (allCountryNames[i] === chosenCountry) {
-//         currentGdp.push(allGdp[i]);
-//         currentYear.push(allYear[i]);
-//       }
-//     }
-//   };
-
-//   setBubblePlot('Afghanistan');
-
-//   function setBubblePlot(chosenCountry) {
-//     getCountryData(chosenCountry);
-
-//     var trace1 = {
-//       x: currentYear,
-//       y: currentGdp,
-//       mode: 'lines+markers',
-//       marker: {
-//         size: 12,
-//         opacity: 0.5
-//       }
-//     };
-
-//     var data = [trace1];
-
-//     var layout = {
-//       title: 'Line and Scatter Plot',
-//       height: 400,
-//       width: 480
-//     };
-
-//     Plotly.newPlot('countryDiv', data, layout);
-//   };
-
-//   var innerContainer = document.querySelector('[data-num="1"'),
-//     // plotEl = innerContainer.querySelector('.plot'),
-//     countrySelector = innerContainer.querySelector('.countrydata');
-
-//   function assignOptions(textArray, selector) {
-//     for (var i = 0; i < textArray.length; i++) {
-//       var currentOption = document.createElement('option');
-//       currentOption.text = textArray[i];
-//       selector.appendChild(currentOption);
-//     }
-//   }
-
-//   assignOptions(listofCountries, countrySelector);
-
-//   function updateCountry() {
-//     setBubblePlot(countrySelector.value);
-//   }
-
-//   countrySelector.addEventListener('change', updateCountry, false);
-// });
-//////////////////////////////////    End of Countries Example   ///////////////////////////////////////////////////////////////////////////////
-
-
-
-  //////////////////////////////////// Select example /////////////////////////////////////////////////////
-  // var graphDiv = document.getElementById('myDiv_select');
-  // var N = 1000;
-  // var color1 = '#7b3294';
-  // var color1Light = '#c2a5cf';
-  // var colorX = '#ffa7b5';
-  // var colorY = '#fdae61';
-
-  // function randomArray() {
-  //   var out = new Array(N);
-  //   for (var i = 0; i < N; i++) {
-  //     out[i] = Math.random();
-  //   }
-  //   return out;
-  // }
-  // var x = randomArray();
-  // var y = randomArray();
-
-  // Plotly.newPlot(graphDiv, [{
-  //   type: 'scatter',
-  //   mode: 'markers',
-  //   x: x,
-  //   y: y,
-  //   xaxis: 'x',
-  //   yaxis: 'y',
-  //   name: 'random data',
-  //   marker: { color: color1, size: 10 }
-  // }, {
-  //   type: 'histogram',
-  //   x: x,
-  //   xaxis: 'x2',
-  //   yaxis: 'y2',
-  //   name: 'x coord dist.',
-  //   marker: { color: colorX }
-  // }, {
-  //   type: 'histogram',
-  //   x: y,
-  //   xaxis: 'x3',
-  //   yaxis: 'y3',
-  //   name: 'y coord dist.',
-  //   marker: { color: colorY }
-  // }], {
-  //   title: 'Lasso around the scatter points to see sub-distributions',
-  //   dragmode: 'lasso',
-  //   xaxis: {
-  //     zeroline: false,
-  //   },
-  //   yaxis: {
-  //     domain: [0.55, 1],
-  //   },
-  //   xaxis2: {
-  //     domain: [0, 0.45],
-  //     anchor: 'y2',
-  //   },
-  //   yaxis2: {
-  //     domain: [0, 0.45],
-  //     anchor: 'x2'
-  //   },
-  //   xaxis3: {
-  //     domain: [0.55, 1],
-  //     anchor: 'y3'
-  //   },
-  //   yaxis3: {
-  //     domain: [0, 0.45],
-  //     anchor: 'x3'
-  //   }
-  // });
-
-  // graphDiv.on('plotly_selected', function (eventData) {
-  //   var x = [];
-  //   var y = [];
-
-  //   var colors = [];
-  //   for (var i = 0; i < N; i++) colors.push(color1Light);
-
-  //   eventData.points.forEach(function (pt) {
-  //     x.push(pt.x);
-  //     y.push(pt.y);
-  //     colors[pt.pointNumber] = color1;
-  //   });
-
-  //   Plotly.restyle(graphDiv, {
-  //     x: [x, y],
-  //     xbins: {}
-  //   }, [1, 2]);
-
-  //   Plotly.restyle(graphDiv, 'marker.color', [colors], [0]);
-  // });
-  /////////////////////////////    End of Selection Example   ///////////////////////////////////////////////////////////////////////////////
-// })
-// Plotly.d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv', function (err, rows) {
-//   function unpack(rows, key) {
-//     return rows.map(function (row) { return row[key]; });
-//   }
-
-//   const arrAvg = arr => arr.reduce((a, b) => a + b, 0) / arr.length
-
-//   var button_layer_2_height = 1.2
-//   var high = unpack(rows, 'AAPL.High').map(x => parseFloat(x))
-//   var low = unpack(rows, 'AAPL.Low').map(x => parseFloat(x))
-//   var date = unpack(rows, 'Date')
-
-
-//   var high_ave = arrAvg(high)
-//   var high_max = Math.max(...high)
-//   var low_ave = arrAvg(low)
-//   var low_min = Math.min(...low)
-
-//   var data = [{
-//     x: date,
-//     y: high,
-//     mode: 'lines',
-//     name: 'High',
-//     marker: { color: '#33CFA5' }
-//   },
-//   {
-//     x: date,
-//     y: date.map(a => high_ave),
-//     mode: 'lines',
-//     name: 'Low Average',
-//     line: { color: '#33CFA5', dash: 'dash' },
-//     visible: false
-//   },
-//   {
-//     x: date,
-//     y: low,
-//     name: 'Low',
-//     mode: 'lines',
-//     marker: { color: '#F06A6A' }
-//   },
-//   {
-//     x: date,
-//     y: date.map(a => low_ave),
-//     mode: 'lines',
-//     name: 'High Average',
-//     visible: false,
-//     line: { color: '#F06A6A', dash: 'dash' }
-//   },
-
-//   ]
-
-//   var high_annotations = [
-//     {
-//       text: 'High Average:<br>' + high_ave.toFixed(2),
-//       x: '2016-03-01',
-//       y: high_ave,
-//       yref: 'y', xref: 'x',
-//       ay: -40, ax: 0
-//     },
-//     {
-//       text: 'High Max:<br>' + high_max.toFixed(2),
-//       x: date[high.indexOf(high_max)],
-//       y: high_max,
-//       yref: 'y', xref: 'x',
-//       ay: -40, ax: 0
-//     },
-//   ]
-
-//   var low_annotations = [{
-//     text: 'Low Average:<br>' + low_ave.toFixed(2),
-//     x: '2015-05-01',
-//     y: low_ave,
-//     yref: 'y', xref: 'x',
-//     ay: 40, ax: 0
-//   },
-//   {
-//     text: 'Low Min:<br>' + low_min.toFixed(2),
-//     x: date[low.indexOf(low_min)],
-//     y: low_min,
-//     yref: 'y', xref: 'x',
-//     ay: 40, ax: 0
-//   }
-//   ]
-
-//   var updatemenus = [
-//     {
-//       buttons: [
-//         {
-//           args: [{ 'visible': [true, false, false, false] },
-//           {
-//             'title': 'Yahoo High',
-//             'annotations': high_annotations
-//           }],
-//           label: 'High',
-//           method: 'update'
-//         },
-//         {
-//           args: [{ 'visible': [false, false, true, true,] },
-//           {
-//             'title': 'Yahoo Low',
-//             'annotations': low_annotations
-//           }],
-//           label: 'Low',
-//           method: 'update'
-//         },
-//         {
-//           args: [{ 'visible': [true, true, true, true,] },
-//           {
-//             'title': 'Yahoo',
-//             'annotations': [...low_annotations, ...high_annotations]
-//           }],
-//           label: 'Both',
-//           method: 'update'
-//         },
-//         {
-//           args: [{ 'visible': [true, false, true, false,] },
-//           {
-//             'title': 'Yahoo',
-//             'annotations': []
-//           }],
-//           label: 'Reset',
-//           method: 'update'
-//         },
-
-//       ],
-//       direction: 'left',
-//       pad: { 'r': 10, 't': 10 },
-//       showactive: true,
-//       type: 'buttons',
-//       x: 0.1,
-//       xanchor: 'left',
-//       y: button_layer_2_height,
-//       yanchor: 'top'
-//     },
-
-//   ]
-
-//   var layout = {
-//     title: 'Yahoo',
-//     updatemenus: updatemenus,
-//     showlegend: false
-//   }
-
