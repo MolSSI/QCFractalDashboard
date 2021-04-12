9b73cec0ce6f66043209f8ea90b89b8eaf250f58
Eman Hussein <emanhussein@Emans-MacBook-Air.local>
2021-04-12 12:11:44 -0400

    removing python code related to unwanted tabs

--- app/routes/projects/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-projects = Blueprint('projects', __name__)
-
-from . import views
--- app/routes/projects/views.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template
-
-from . import projects
-
-
-@projects.route("/views/projects")
-def project_list():
-    return render_template("projects/project_list.html")
